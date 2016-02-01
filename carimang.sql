403f9e558ca123df3fc8d8967dbae790e7991fc8
Ahmad N. F <fahmadnaufal@gmail.com>
2016-02-01 11:06:10 +0700

    database: add foreign keys

--- carimang.sql
+++ carimang.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 30, 2016 at 09:34 AM
+-- Generation Time: Feb 01, 2016 at 05:04 AM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -114,6 +114,13 @@ CREATE TABLE `ruangan_perbaikan` (
 --
 
 --
+-- Indexes for table `acara_kegiatan`
+--
+ALTER TABLE `acara_kegiatan`
+  ADD KEY `id_kegiatan` (`id_kegiatan`),
+  ADD KEY `id_ruangan` (`id_ruangan`);
+
+--
 -- Indexes for table `kegiatan`
 --
 ALTER TABLE `kegiatan`
@@ -132,10 +139,24 @@ ALTER TABLE `perbaikan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  ADD KEY `id_ruangan` (`id_ruangan`),
+  ADD KEY `id_kuliah` (`id_kuliah`);
+
+--
 -- Indexes for table `ruangan`
 --
 ALTER TABLE `ruangan`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ruangan_perbaikan`
+--
+ALTER TABLE `ruangan_perbaikan`
+  ADD KEY `id_ruangan` (`id_ruangan`),
+  ADD KEY `id_perbaikan` (`id_perbaikan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -161,6 +182,31 @@ ALTER TABLE `perbaikan`
 --
 ALTER TABLE `ruangan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `acara_kegiatan`
+--
+ALTER TABLE `acara_kegiatan`
+  ADD CONSTRAINT `acara_kegiatan_ibfk_1` FOREIGN KEY (`id_kegiatan`) REFERENCES `kegiatan` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `acara_kegiatan_ibfk_2` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `perkuliahan`
+--
+ALTER TABLE `perkuliahan`
+  ADD CONSTRAINT `perkuliahan_ibfk_1` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `perkuliahan_ibfk_2` FOREIGN KEY (`id_kuliah`) REFERENCES `kuliah` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `ruangan_perbaikan`
+--
+ALTER TABLE `ruangan_perbaikan`
+  ADD CONSTRAINT `ruangan_perbaikan_ibfk_1` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ruangan_perbaikan_ibfk_2` FOREIGN KEY (`id_perbaikan`) REFERENCES `perbaikan` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
