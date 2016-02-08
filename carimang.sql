3a407c3585d11dbf024fea914c42b8caba9ab15e
Ahmad N. F <fahmadnaufal@gmail.com>
2016-02-08 14:25:35 +0700

    chg: relation 'kegiatan' to 'peminjam', relation 'acara_kegiatan' to 'kegiatan'

--- carimang.sql
+++ carimang.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2016 at 07:50 AM
+-- Generation Time: Feb 08, 2016 at 08:24 AM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -23,28 +23,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `acara_kegiatan`
---
-
-CREATE TABLE `acara_kegiatan` (
-  `id_kegiatan` int(11) NOT NULL,
-  `id_ruangan` int(11) NOT NULL,
-  `tanggal` date NOT NULL,
-  `waktu_mulai` time NOT NULL,
-  `waktu_selesai` time NOT NULL,
-  `penanggung_jawab` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `kegiatan`
 --
 
 CREATE TABLE `kegiatan` (
-  `id` int(11) NOT NULL,
+  `id_peminjam` int(11) NOT NULL,
   `nama_kegiatan` varchar(256) NOT NULL,
-  `nama_penyelenggara` varchar(256) NOT NULL
+  `tanggal` date NOT NULL,
+  `waktu_mulai` time NOT NULL,
+  `waktu_selesai` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -63,11 +50,22 @@ CREATE TABLE `kuliah` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `peminjam`
+--
+
+CREATE TABLE `peminjam` (
+  `id` int(11) NOT NULL,
+  `nama_peminjam` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `perbaikan`
 --
 
 CREATE TABLE `perbaikan` (
-  `id` int(11) NOT NULL,
+  `nama_ruangan` varchar(256) NOT NULL,
   `tanggal_mulai` date NOT NULL,
   `tanggal_selesai` date NOT NULL,
   `deskripsi` text NOT NULL
@@ -80,8 +78,7 @@ CREATE TABLE `perbaikan` (
 --
 
 CREATE TABLE `perkuliahan` (
-  `id_kuliah` int(11) NOT NULL,
-  `id_ruangan` int(11) NOT NULL,
+  `kode_kuliah` varchar(64) NOT NULL,
   `tanggal` date NOT NULL,
   `waktu_mulai` time NOT NULL,
   `waktu_selesai` time NOT NULL,
@@ -101,84 +98,61 @@ CREATE TABLE `ruangan` (
   `tipe_ruangan` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `ruangan_perbaikan`
---
-
-CREATE TABLE `ruangan_perbaikan` (
-  `id_ruangan` int(11) NOT NULL,
-  `id_perbaikan` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `acara_kegiatan`
---
-ALTER TABLE `acara_kegiatan`
-  ADD KEY `id_kegiatan` (`id_kegiatan`),
-  ADD KEY `id_ruangan` (`id_ruangan`);
-
---
 -- Indexes for table `kegiatan`
 --
 ALTER TABLE `kegiatan`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `id_peminjam` (`id_peminjam`);
 
 --
 -- Indexes for table `kuliah`
 --
 ALTER TABLE `kuliah`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kode_kuliah` (`kode_kuliah`);
+
+--
+-- Indexes for table `peminjam`
+--
+ALTER TABLE `peminjam`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `perbaikan`
 --
 ALTER TABLE `perbaikan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`nama_ruangan`);
 
 --
 -- Indexes for table `perkuliahan`
 --
 ALTER TABLE `perkuliahan`
-  ADD KEY `id_ruangan` (`id_ruangan`),
-  ADD KEY `id_kuliah` (`id_kuliah`);
+  ADD KEY `kode_kuliah` (`kode_kuliah`);
 
 --
 -- Indexes for table `ruangan`
 --
 ALTER TABLE `ruangan`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ruangan_perbaikan`
---
-ALTER TABLE `ruangan_perbaikan`
-  ADD KEY `id_ruangan` (`id_ruangan`),
-  ADD KEY `id_perbaikan` (`id_perbaikan`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama_ruangan` (`nama_ruangan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `kegiatan`
---
-ALTER TABLE `kegiatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `kuliah`
 --
 ALTER TABLE `kuliah`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `perbaikan`
+-- AUTO_INCREMENT for table `peminjam`
 --
-ALTER TABLE `perbaikan`
+ALTER TABLE `peminjam`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `ruangan`
@@ -190,37 +164,16 @@ ALTER TABLE `ruangan`
 --
 
 --
--- Constraints for table `acara_kegiatan`
+-- Constraints for table `kegiatan`
 --
-ALTER TABLE `acara_kegiatan`
-  ADD CONSTRAINT `acara_kegiatan_ibfk_1` FOREIGN KEY (`id_kegiatan`) REFERENCES `kegiatan` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `acara_kegiatan_ibfk_2` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `kuliah`
---
-ALTER TABLE `kuliah`
-  ADD UNIQUE (`kode_kuliah`);
+ALTER TABLE `kegiatan`
+  ADD CONSTRAINT `kegiatan_ibfk_1` FOREIGN KEY (`id_peminjam`) REFERENCES `peminjam` (`id`);
 
 --
 -- Constraints for table `perkuliahan`
 --
 ALTER TABLE `perkuliahan`
-  ADD CONSTRAINT `perkuliahan_ibfk_1` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `perkuliahan_ibfk_2` FOREIGN KEY (`id_kuliah`) REFERENCES `kuliah` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `ruangan`
---
-ALTER TABLE `ruangan`
-  ADD UNIQUE (`nama_ruangan`);
-
---
--- Constraints for table `ruangan_perbaikan`
---
-ALTER TABLE `ruangan_perbaikan`
-  ADD CONSTRAINT `ruangan_perbaikan_ibfk_1` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `ruangan_perbaikan_ibfk_2` FOREIGN KEY (`id_perbaikan`) REFERENCES `perbaikan` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `perkuliahan_ibfk_1` FOREIGN KEY (`kode_kuliah`) REFERENCES `kuliah` (`kode_kuliah`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
