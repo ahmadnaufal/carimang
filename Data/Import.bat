8829ce5d932821b1e906e439de2e0b02666abcdf
Muhamad Visat Sutarno <mvisat@gmail.com>
2016-02-15 17:13:09 +0700

    add dummy data

---
+++ Data/Import.bat
@@ -0,0 +1,2 @@
+mysqlimport --ignore-lines=1 --fields-terminated-by=, --local -u root -p -v carimang "Ruangan.csv"
+mysqlimport --ignore-lines=1 --fields-terminated-by=, --local -u root -p -v carimang "Kuliah.csv"
